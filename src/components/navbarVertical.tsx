cb3b236a6636da6031043723cb1d1b79528c134a
Victor Santos <santos.victors2000@gmail.com>
2025-10-09 14:40:02 -0300

    fix(navigation): corrige import do componente Image

--- src/components/navbarVertical.tsx
+++ src/components/navbarVertical.tsx
@@ -1,11 +1,14 @@
 import { Divider } from "@heroui/divider";
 import { CustomNavLink } from "./customNavLink";
+import { Image } from "@heroui/react";
+import icon_7pecados from "../../public/icons/7pecados.png";
 
 export const NavbarVertical = () => {
   return (
     <div className="col-span-3 lg:col-span-2">
       <section className="flex flex-col w-full h-full justify-between p-6 items-center">
         <div className="">
+          <Image alt="HeroUI hero Image" src={icon_7pecados} width={106} />
           <Divider className="my-4" />
         </div>
         <nav className="flex flex-col items-start h-full">
