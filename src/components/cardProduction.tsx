39f02c4186088d89a598847d413fa7758eff87e8
Victor Santos <santos.victors2000@gmail.com>
2025-10-21 18:09:14 -0300

    feat: add initial component cardProduction

---
+++ src/components/cardProduction.tsx
@@ -0,0 +1,79 @@
+import { ReactNode } from "react";
+import { Button } from "@heroui/button";
+
+type CardProductionProps = {
+  id: string;
+  place_to_buy: string;
+  name_user: string;
+  address: string;
+  type_payment: string;
+  total_value: string;
+  status: string;
+  children: ReactNode;
+};
+
+export const CardProduction = ({
+  id,
+  place_to_buy,
+  name_user,
+  address = "",
+  type_payment,
+  total_value,
+  status,
+  children,
+}: CardProductionProps) => {
+  let type_color;
+
+  place_to_buy === "Loja"
+    ? (type_color = "primary")
+    : (type_color = "secondary");
+
+  let title_button;
+  switch (status) {
+    case "pending":
+      title_button = "Enviar para produção";
+      break;
+    case "in_production":
+      title_button = "Finalizar pedido";
+      break;
+    case "ready":
+      title_button = "Entregue";
+      break;
+  }
+  return (
+    <div className="flex flex-col bg-base px-6 py-8 gap-4 shadow-2xs rounded-xl mx-2 mt-3">
+      <div className="flex w-full justify-start">
+        <div
+          className={`bg-${type_color} w-fit px-4 py-1 rounded-xl text-white text-[0.75rem]`}
+        >
+          {place_to_buy}
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className={`flex flex-col text-${type_color}`}>
+          <div className="font-bold text-lg">
+            {name_user} - #{id}
+          </div>
+          <div className="text-xs">{address}</div>
+        </div>
+        <div>{children}</div>
+        <div>
+          <hr className="border-gray-500 border-dashed border-1" />
+          <div
+            className={`text-${type_color} flex flex-row justify-between text-sm`}
+          >
+            <div>{type_payment}</div>
+            <div>{total_value}</div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Button
+          className={`bg-primary text-base rounded-xl bg-${type_color} flex w-full`}
+        >
+          <span className="default invert">{title_button}</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
--- src/pages/production.tsx
+++ src/pages/production.tsx
@@ -2,15 +2,62 @@ import DefaultLayout from "@/layouts/default";
 import { Input } from "@heroui/input";
 import { Search } from "lucide-react";
 import { ColumnsProduction } from "@/components/columnsProduction";
+import { CardProduction } from "@/components/cardProduction";
+
+const list_productions = [
+  {
+    id: "1251",
+    place_to_buy: "Loja",
+    name_user: "Nathalia Gomes",
+    address: "Rua agostinho Gama - 42",
+    type_payment: "Pagamento no cartão",
+    total_value: "R$26,58",
+    status: "pending",
+  },
+  {
+    id: "1321",
+    place_to_buy: "Loja",
+    name_user: "Nathalia Gomes",
+    address: "Rua agostinho Gama - 42",
+    type_payment: "Pagamento no cartão",
+    total_value: "R$30,58",
+    status: "in_production",
+  },
+  {
+    id: "1344",
+    place_to_buy: "Entrega",
+    name_user: "Nathalia Gomes",
+    address: "Rua agostinho Gama - 42",
+    type_payment: "Pagamento no cartão",
+    total_value: "R$30,58",
+    status: "in_production",
+  },
+  {
+    id: "1322",
+    place_to_buy: "Entrega",
+    name_user: "Nathalia Gomes",
+    address: "Rua agostinho Gama - 42",
+    type_payment: "Pagamento no pix",
+    total_value: "R$50,58",
+    status: "ready",
+  },
+];
 
 export default function ProductionPage() {
+  const pending_list_filter_production = list_productions.filter(
+    (production) => production.status === "pending"
+  );
+  const in_production_list_filter_production = list_productions.filter(
+    (production) => production.status === "in_production"
+  );
+  const ready_list_filter_production = list_productions.filter(
+    (production) => production.status === "ready"
+  );
+
   return (
     <DefaultLayout>
       <main className="flex flex-col gap-6">
         <section className="flex gap-4">
-          <div className="h-full bg-primary flex p-4 rounded-2xl text-base font-bold">
-            1/6
-          </div>
           <div className="max-w-[320px]">
             <Input
               className="bg-base shadow-2xl rounded-xl"
@@ -20,6 +67,7 @@ export default function ProductionPage() {
               startContent={<Search />}
             />
           </div>
+          <div>Botão aqui do lado : )</div>
         </section>
         <section className="grid grid-cols-4">
           <ColumnsProduction
@@ -27,21 +75,58 @@ export default function ProductionPage() {
             quantityOfItems={"1"}
             type_rounded={"rounded-tl-xl"}
           >
-            A
+            {pending_list_filter_production.map((element) => (
+              <CardProduction
+                id={element.id}
+                place_to_buy={element.place_to_buy}
+                name_user={element.name_user}
+                address={element.address}
+                type_payment={element.type_payment}
+                total_value={element.total_value}
+                status={element.status}
+              >
+                ITEM AQUI
+              </CardProduction>
+            ))}
           </ColumnsProduction>
+
           <ColumnsProduction
             title={"Em produção"}
             quantityOfItems={"2"}
             type_rounded={"none"}
           >
-            B
+            {in_production_list_filter_production.map((element) => (
+              <CardProduction
+                id={element.id}
+                place_to_buy={element.place_to_buy}
+                name_user={element.name_user}
+                address={element.address}
+                type_payment={element.type_payment}
+                total_value={element.total_value}
+                status={element.status}
+              >
+                ITEM AQUI
+              </CardProduction>
+            ))}
           </ColumnsProduction>
           <ColumnsProduction
             title={"Prontos"}
             quantityOfItems={"3"}
             type_rounded={"none"}
           >
-            C
+            {ready_list_filter_production.map((element) => (
+              <CardProduction
+                id={element.id}
+                place_to_buy={element.place_to_buy}
+                name_user={element.name_user}
+                address={element.address}
+                type_payment={element.type_payment}
+                total_value={element.total_value}
+                status={element.status}
+              >
+                ITEM AQUI
+              </CardProduction>
+            ))}
           </ColumnsProduction>
           <ColumnsProduction
             title={"Entregues"}
