7a169846b1a00a93c134efa20d340194c0de421a
Victor Santos <santos.victors2000@gmail.com>
2025-10-21 15:04:14 -0300

    feat: add layout initial to page production

--- src/pages/production.tsx
+++ src/pages/production.tsx
@@ -1,9 +1,27 @@
 import DefaultLayout from "@/layouts/default";
+import { Input } from "@heroui/input";
+import { Search } from "lucide-react";
 
 export default function ProductionPage() {
   return (
     <DefaultLayout>
-      <div>Produção</div>
+      <main className="flex flex-col gap-6">
+        <section className="flex gap-4">
+          <div className="h-full bg-primary flex p-4 rounded-2xl text-base font-bold">
+            1/6
+          </div>
+          <div className="max-w-[320px]">
+            <Input
+              className="bg-base shadow-2xl rounded-xl"
+              label="Pesquisar Cliente"
+              placeholder="Digite aqui..."
+              type="email"
+              startContent={<Search />}
+            />
+          </div>
+        </section>
+        <section>Grid</section>
+      </main>
     </DefaultLayout>
   );
 }
