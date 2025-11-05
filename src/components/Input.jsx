43da0c3b6089eb77f1e6f9ebef1da380e694e216
Victor Santos <santos.victors2000@gmail.com>
2025-11-04 23:04:11 -0300

    feat: Adicionado componente de input utilizando props para receber valores e adicionar no seu elemento

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,11 @@
-const Input = ({ label, ...rest }) => {
+const Input = ({ label, className, ...rest }) => {
   return (
     <div className="flex flex-col space-y-1 text-left">
       <label className="text-sm font-semibold text-[#35383E]" htmlFor={rest.id}>
         {label}
       </label>
       <input
-        className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]"
+        className={`rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F] ${className}`}
         {...rest}
       />
     </div>
