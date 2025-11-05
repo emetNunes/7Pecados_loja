2747c3c2978fd5e1b5de19c09785d502b3f41b27
Victor Santos <santos.victors2000@gmail.com>
2025-11-04 22:58:25 -0300

    feat: criado compontes em jsx, com as mesmas configurações de antes

---
+++ src/components/cardAccess.jsx
@@ -0,0 +1,23 @@
+import { Button } from "@heroui/react";
+
+export const CardAccess = ({
+  title,
+  description,
+  clickbutton = false,
+  children = false,
+}) => {
+  return (
+    <div className="flex flex-col bg-base px-6 py-8 gap-8 shadow-2xs rounded-xl w-full">
+      <div className="text-primary font-bold text-xl">{title}</div>
+      <div className="defaut text-sm">{description}</div>
+      {clickbutton}
+      {!children ? (
+        ""
+      ) : (
+        <Button className="bg-primary text-base rounded-xl">
+          <span className="default invert">{children}</span>
+        </Button>
+      )}
+    </div>
+  );
+};
